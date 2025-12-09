0e516394ec1b7edfb6bdbd25a17637f519e2a51d
rayhan889 <raydev1604@gmail.com>
2025-12-10 00:52:02 +0700

    feat: api docs w swagger & scalar

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,16 @@ import (
 	"github.com/rayhan889/neatspace/cmd/commands"
 )
 
+// @title	    Neatspace API
+// @description	Neatspace API documentation
+// @version		1.0
+
+// @securityDefinitions.http bearerAuth
+// @scheme bearer
+// @bearerFormat JWT
+
+// @host      localhost:8080
+// @BasePath  /api/v1
 func main() {
 	if err := commands.RootCmd.Execute(); err != nil {
 		fmt.Println(err)
--- internal/application/handler/user.go
+++ internal/application/handler/user.go
@@ -39,6 +39,21 @@ func NewUserHandler(opts UserHandlerOpts) {
 	g.Post("", middlewares.ValidateRequestJSON[dto.CreateUser](), h.CreateUser)
 }
 
+// PaginationUser godoc
+// @Summary 		Pagination Users
+// @Description 	Paginating Through List of Users
+// @Tags 			Users
+// @Accept 			json
+// @Produce 		json
+// @Param			page		query	int		false	"Page number (default: 1, min: 1)"				default(1)		minimum(1)
+// @Param			per_page	query	int		false	"Items per page (default: 10, max: 100)"		default(10)		minimum(1)	maximum(100)
+// @Param			search		query	string	false	"Search by display name or username"
+// @Param			role		query	string	false	"Filter by role (user or admin)"				Enums(user, admin)
+// @Success      	200   {object}  apputils.PaginationResponse[dto.UserPagination]
+// @Failure      	400   {object}  apputils.BaseResponse
+// @Failure      	404   {object}  apputils.BaseResponse
+// @Failure      	500   {object}  apputils.BaseResponse
+// @Router       	/api/v1/users [get]
 func (h *UserHandler) PaginationUser(c *fiber.Ctx) error {
 	p := apputils.Paginate(c)
 
@@ -52,6 +67,18 @@ func (h *UserHandler) PaginationUser(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusOK).JSON(apputils.SuccessResponse(apputils.PaginationBuilder(data, *meta)))
 }
 
+// CreateUser godoc
+// @Summary 		Create User
+// @Description 	Create a new user account
+// @Tags 			Users
+// @Accept 			json
+// @Produce 		json
+// @Param			request	body	dto.CreateUser	true	"User creation request"
+// @Success      	201
+// @Failure      	400   {object}  apputils.BaseResponse
+// @Failure      	422   {object}  apputils.BaseResponse
+// @Failure      	500   {object}  apputils.BaseResponse
+// @Router       	/api/v1/users [post]
 func (h *UserHandler) CreateUser(c *fiber.Ctx) error {
 	req := c.Locals(constants.RequestBodyJSONKey).(*dto.CreateUser)
 
